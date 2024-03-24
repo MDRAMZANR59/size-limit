3d266558ac6e6b5d1ac2bb10084d9a3ab0d14c0a
MDRAMZANR59 <mdramzanaliisdbr59@gmail.com>
2024-03-24 22:35:40 +0600

    prime, composit edit

--- practis array.js
+++ practis array.js
@@ -1,20 +1,23 @@
-var x=parseFloat(prompt('Enter Number'))
+// var x=parseFloat(prompt('Enter Number'))
 
-if(x%2===1){
-    console.log(x+' is Even Number');
-}else if(x%2===0){
-    console.log(x+' is Odd Number');
-}else{
-    console.log('Wrong Input');
-}
+// if(x%2===1){
+//     console.log(x+' is Even Number');
+// }else if(x%2===0){
+//     console.log(x+' is Odd Number');
+// }else{
+//     console.log('Wrong Input');
+// }
 
 var x=Math.round(prompt('Enter Number'))
 var y=Math.abs(prompt('Enter Second Number'))
 
 if(x%y===0){
-    console.log(x+' is Prime Number');
+        console.log(x+' is Composit Number');
+    
 }else if(x%y>0){
-    console.log(x+' is Composit Number');
+    console.log(x+' is Prime Number');
 }else{
     console.log('Wrong Input');
 }
+// var x=10
+// while(x)
