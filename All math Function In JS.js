a534d2d3922a0e7ffdb27927d0e9ab4a5f0ee6dc
MDRAMZANR59 <mdramzanaliisdbr59@gmail.com>
2024-03-25 20:23:01 +0600

    All Math Function In JS

---
+++ All math Function In JS.js
@@ -0,0 +1,15 @@
+var x=10.49
+var y=50
+var z=30
+
+console.log(Math.abs(x));
+console.log(Math.ceil(x));
+console.log(Math.floor(x));
+console.log(Math.round(x));
+console.log(Math.max(x,y,z));
+console.log(Math.min(x,y,z));
+console.log(Math.pow(x,y));
+console.log(Math.sqrt(x,y));
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.round(Math.random()*500));
