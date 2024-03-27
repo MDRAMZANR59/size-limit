b1928766d938b2f5dbd4c47c0b993b45b0e518c5
MDRAMZANR59 <mdramzanaliisdbr59@gmail.com>
2024-03-27 07:57:14 +0600

    Some Nested Loop

--- C1/JaSc.js
+++ C1/JaSc.js
@@ -6,42 +6,42 @@
 
 // var x='fdfd'
 
-if(x%2===0){
-    console.log('This Even Number');
-}else if(x%2===1){
-    console.log('This Odd Number');
-}else{
-    console.log('Wrong Input');
-}
+// if(x%2===0){
+//     console.log('This Even Number');
+// }else if(x%2===1){
+//     console.log('This Odd Number');
+// }else{
+//     console.log('Wrong Input');
+// }
 
-var y=(x%2===0) ? 'This Even Number' : (x%2===1)? 'This Odd Number':'Wrong Input'
-
-
-
-function fdgfd(n){
-    var num=1
-    for(x=1;x<=n;x++){
-        z=num*x
-        results=num+'*'+x+' ='+(z)
-        num=z
-        console.log(results);
-    }
-}
-
-fdgfd(3)
+// var y=(x%2===0) ? 'This Even Number' : (x%2===1)? 'This Odd Number':'Wrong Input'
 
 
-var nm='Akash'
-while (nm) {
-    var r = Math.floor(Math.random()*10+1)
-    if (r === 9) {
-       console.log(' You Win')
-        break
-    } else {
-        console.log('You Got '+r)
-        continue
-        }
-}
+
+// function fac(n){
+//     var num=1
+//     for(x=1;x<=n;x++){
+//         z=num*x
+//         results=num+'*'+x+' = '+(z)
+//         num=z
+//         console.log(results);
+//     }
+// }
+
+// fac(5)
+
+
+// var nm='Akash'
+// while (nm) {
+//     var r = Math.floor(Math.random()*10+1)
+//     if (r === 9) {
+//        console.log(' You Win')
+//         break
+//     } else {
+//         console.log('You Got '+r)
+//         continue
+//         }
+// }
 
 1
 12
@@ -53,36 +53,68 @@ while (nm) {
 12345678
 123456789
 
-for(x=1;x<=10;x++){
-res=' '
-    for(y=1;y<=x;y++){
-    res+=y+' '
+// for(x=1;x<=10;x++){
+// res=' '
+//     for(y=1;y<=x;y++){
+//     res+=y+' '
     
-    }
-    console.log(res);
-}
-for(x=1;x<=5;x++){
-    var rs=' '
-    for(y=1;y<=x;y++){
-        rs+=' + '
-    }
-    console.log(rs);
-}
-var n=5
-for(x=1;x<=n;x++){
-    store=' '
-    for(y=1;y<=n;y++){
-        store+=' * '
-    }
-    console.log(store);
-}
-var n=5
-var results=' '
-for(x=1;x<=10;x++){
-    var results=n+'*'+x+' ='+(n*x)
-    for(y=1;y<=10;y++){
-        results=+n+' * '+y+' = '+(n*y)
-    }
-    console.log(results);
-}
+//     }
+//     console.log(res);
+// }
+// for(x=1;x<=5;x++){
+//     var rs=' '
+//     for(y=1;y<=x;y++){
+//         rs+=' + '
+//     }
+//     console.log(rs);
+// }
+// var n=5
+// for(x=1;x<=n;x++){
+//     store=' '
+//     for(y=1;y<=n;y++){
+//         store+=' * '
+//     }
+//     console.log(store);
+// }
+// var n=5
+// var results=' '
+// for(x=1;x<=10;x++){
+//     var results=n+'*'+x+' ='+(n*x)
+//     for(y=1;y<=10;y++){
+//         results=+n+' * '+y+' = '+(n*y)
+//     }
+//     console.log(results);
+// }
+// var n=20
+// for(x=1;x<=n;x++){
+//     results=' '
+//     for(y=1;y<=n;y++){
+//         results=results+'* '+' '
+//     }
+//     console.log(results);
+// // }
 
+// function fac(n){
+//     num=1
+//     for(x=1;x<=n;x++){
+//         net=(num*x)
+//         results=num+' * '+x+' = '+net
+//         num=net
+//         console.log(results)
+//     }
+// }
+// fac(6)
+// var n=6
+
+
+// for(x=1;x<=1;x++){
+//     num=1
+//     var res=' '
+//    for(y=1;y<=n;y++){
+//     z=(num*y)
+//     var res=num+' * '+y+' = '+z
+//     num=z
+//     console.log(res);
+//    } 
+  
+// }
