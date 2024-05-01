c5002189e84cf938bce2eb15cb60e97477f50822
MDRAMZANR59 <mdramzanaliisdbr59@gmail.com>
2024-05-01 11:27:59 +0600

    Not Code Duplicate

--- Experiment/Raf/Js.js
+++ Experiment/Raf/Js.js
@@ -1,3 +1,7 @@
+function dry(child,perent){
+    child.prototype=Object.create(perent.prototype)
+    child.prototype.constructor=child
+}
 function Test(h,w){
     this.width=h
     this.height=w
@@ -18,8 +22,7 @@ function Tst1(h,w,w1,h1){
 }
 //Copy Prototype Of Another With Reset Constractor
 
-Tst1.prototype=Object.create(Test.prototype)
-Tst1.prototype.constructor=Tst1
+dry(Tst1,Test)
 
 var c=new Tst1(10,20,30,40)
 //Copy One Constractor To another Constractor
@@ -29,5 +32,6 @@ function Test2(h,w){
         return this.width+this.height
     }
 }
+dry(Test2,Test)
 
 var p=new Test2(2,3)
