4e5bccdea7de189e887a20cf25d3ff8d7a691e76
MDRAMZANR59 <mdramzanaliisdbr59@gmail.com>
2024-05-18 07:52:28 +0600

    Object Assign, Array use As String, Object Enhance

--- Experiment/ES6/js.js
+++ Experiment/ES6/js.js
@@ -1,5 +1,25 @@
-(function ss(){
-    var x=10
-    console.log(x);
-})()
-console.log(x);
+//Array See As A String
+let sprad=[1,2,3]
+console.log(...sprad);
+
+// Object Copy An Another Object
+let obj={
+    x:10,
+    y:20,
+    z:30
+}
+let obj2={
+    ...obj
+}
+console.log(obj2);
+
+// use Variable Name As A Object Property Name
+let x=10;
+let y=20;
+let z=30;
+let objenhn={
+    x,
+    y,
+    z
+}
+console.log(objenhn.y);
